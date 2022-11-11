9220e23f27c0f5d53577ed3f7719283d6a74c47b
Julian Alexander Murillo <julian.alexander.murillo@gmail.com>
2022-11-11 14:21:14 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, FormControlLabel, Radio, RadioGroup, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { useState } from 'react';
+import { generateQuery, searchInGoogle } from './utils';
 
 function App() {
+  const [value, setValue] = useState('agencies');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleClick = (event) => {
+    
+    console.log('Value Selected', value)
+    searchInGoogle({ query: generateQuery({ value }) })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box sx={{ alignItems: 'center', width: '100%', maxWidth: 800, display: 'flex', flexDirection: 'column' }}>
+        <Typography variant="h3" gutterBottom>
+          Buscador del Makeup Artist
+        </Typography>
+
+        <RadioGroup onChange={handleChange} defaultValue="agencies">
+          <FormControlLabel value="agencies" control={<Radio />} label="Agencias" />
+          <FormControlLabel value="runways" control={<Radio />} label="Pasarelas" />
+          <FormControlLabel value="catalogs" control={<Radio />} label="Catalogos" />
+          <FormControlLabel value="events" control={<Radio />} label="Eventos" />
+          <FormControlLabel value="productions" control={<Radio />} label="Producciones" />
+          <FormControlLabel value="academies" control={<Radio />} label="Academias" />
+        </RadioGroup>
+
+        <Button variant="contained" onClick={handleClick}>Buscar</Button>
+
+      </Box>
+
     </div>
   );
 }
---
+++ src/utils.js
@@ -0,0 +1,22 @@
+const URL = 'instagram.com';
+const CITY = 'medellin';
+
+export function generateQuery({ value }) {
+  return {
+    'agencies': `"agencia"|"agency" "modelaje"|"modelos" "${CITY}" inurl:${URL} -webcam"`,
+    'runways': `"pasarelas"|"pasarela" "${CITY}" inurl:${URL} -webcam"`,
+    'catalogs': `"catalogos"|"catalogo" "${CITY}" inurl:${URL} -webcam"`,
+    'events': `"eventos"|"evento" "${CITY}" inurl:${URL} -webcam"`,
+    'productions': `"producciones"|"produccion" "${CITY}" inurl:${URL} -webcam"`,
+    'academies': `"academia"|"academy"|"academias" "arte"|"modelaje"|"danza"|"baile"|"actuacion"|"ballet" "${CITY}" inurl:${URL} -webcam"`,
+  }[value]
+}
+
+export function generateGoogleSearchURL({ query }) {
+  return `https://www.google.com/search?q=${query}`
+}
+
+export function searchInGoogle({ query }) {
+  window.open(generateGoogleSearchURL({ query }), '__blank');
+  return true;
+}
